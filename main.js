a48763133e6834f46241e0aa78ba3344814d3b05
Mousumi <dutta.mousumi136@gmail.com>
2024-02-26 12:39:08 -0500

    primitive constructive functions

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("Hello world");
+let rose = 8;
+let rose_quantity = 70;
+let lily = 10;
+let lily_quantity = 50;
+let tulip = 2;
+let tulip_quantity = 120;
+
+function flowerValue(piece,amount){
+   return piece * amount;
+}
+
+console.log(flowerValue(rose,rose_quantity));
+
+console.log(flowerValue(lily,lily_quantity));
+
+console.log(flowerValue(tulip,tulip_quantity));
+
+let  name1='Maxwell Wright';
+let  name2='Raja Villarreal';
+let  name3='Helen Richards';
+
+let phone1='(0191) 719 6495';
+let phone2='0866 398 2895';
+let phone3='0800 1111';
+
+let email1='Curabitur.egestas.nunc@nonummyac.co.uk';
+let email2='posuere.vulputate@sed.com';
+let email3='libero@convallis.edu';
+
+console.log(`${name1}/${phone1}/${email1}`);
+console.log(`${name2}/${phone2}/${email2}`);
+console.log(`${name3}/${phone3}/${email3}`);
+console.log(typeof console.log)
+
+year=2024
+console.log(typeof year)
+let typeYear = typeof year
+console.log(typeYear)
+console.log(typeof typeYear)
+
+let path="C:\\\\Windows"
+console.log(path)
+
+console.log(email1-email2)//NaN
+console.log('10' - '12')
+
+console.count("Hello");
+console.time();
+console.timeEnd()
+
+console.log('hello'.slice(1,5))
+let variable1;
+console.log(typeof null)//type is 'object'
+console.log(typeof variable1)//type is 'undefined'
